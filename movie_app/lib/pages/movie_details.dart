b530d7e8acf3246e095b3389d987483ea1d29a05
green <kirakbit@gmail.com>
2024-06-06 15:30:05 -0500

    section 9 74

--- movie_app/lib/pages/movie_details.dart
+++ movie_app/lib/pages/movie_details.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/movie_card.dart';
 import 'package:movie_app/models/movie.dart';
 
 class MovieDetails extends StatelessWidget {
   const MovieDetails({super.key, required this.movie});
   final Movie movie;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(movie.title)),
+      appBar: AppBar(title: Text("Movie Details")),
+      body: Container(
+        child: Column(
+          children: [
+            MovieCard(movie: movie),
+            Text("Movie Posters",
+                style: Theme.of(context).textTheme.headlineSmall),
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: movie.images.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 4.0,
+                      child: Image.network(movie.images[index]),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
